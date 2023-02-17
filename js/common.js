75c04bd8137b52828e7da21188a22a373879bad5
ghl <15938837525@qq.com>
2023-02-17 20:45:26 +0800

    初始化代码

---
+++ js/common.js
@@ -0,0 +1,9 @@
+// 上面这个代码处理过度动画（默认加上不用管）
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    document.body.classList.add('sidenav-pinned')
+    document.body.classList.add('ready')
+  }, 200)
+})
+
+// 每个页面都会导入该js 统一的设置写这里即可
