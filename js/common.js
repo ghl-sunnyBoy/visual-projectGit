fdc40dc824b06de2fbaf715e95f40b4a2dcdf5cd
ghl <15938837525@qq.com>
2023-02-18 19:56:21 +0800

    响应拦截

--- js/common.js
+++ js/common.js
@@ -7,3 +7,23 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 // 每个页面都会导入该js 统一的设置写这里即可
+
+// 弹框
+function tiy(str) {
+  myToast.children[0].innerHTML = str
+  const fm = new bootstrap.Toast(myToast, {
+    delay: 1000
+  })
+  fm.show()
+}
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return response.data;
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
